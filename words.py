1a9f0de77c1ef39a9273f464738556d205de9b25
Pat Jensen <patj@jensencloud.net>
2021-09-19 18:12:42 -0700

    bounds detection complete

--- words.py
+++ words.py
@@ -246,48 +246,36 @@ def Main(window):
   while key != ord('q') and key != 27:
     if key == curses.KEY_UP:
       cy, cx = curses.getsyx()
-      if cy < 4:
-        curses.beep()
-        curses.beep()
-        curses.beep()
-      try:
-        window.move(cy-1, cx)
-      except:
-        curses.beep()
-        window.refresh()
+      if not cy < 4:
+        try:
+          window.move(cy-1, cx)
+        except:
+          curses.beep()
+          window.refresh()
     elif key == curses.KEY_DOWN:
       cy, cx = curses.getsyx()
-      if cy > 16:
-        curses.beep()
-        curses.beep()
-        curses.beep()
-      try:
-        window.move(cy+1, cx)
-      except:
-        curses.beep()
-        window.refresh()
+      if not cy > 16:
+        try:
+          window.move(cy+1, cx)
+        except:
+          curses.beep()
+          window.refresh()
     elif key == curses.KEY_LEFT:
       cy, cx = curses.getsyx()
-      if cx < 27:
-        curses.beep()
-        curses.beep()
-        curses.beep()
-      try:
-        window.move(cy, cx-2)
-      except:
-        curses.beep()
-        window.refresh()
+      if not cx < 27:
+        try:
+          window.move(cy, cx-2)
+        except:
+          curses.beep()
+          window.refresh()
     elif key == curses.KEY_RIGHT:
       cy, cx = curses.getsyx()
-      if cx > 52:
-        curses.beep()
-        curses.beep()
-        curses.beep()
-      try:
-        window.move(cy, cx+2)
-      except:
-        curses.beep()
-        window.refresh()
+      if not cx > 52:
+        try:
+          window.move(cy, cx+2)
+        except:
+          curses.beep()
+          window.refresh()
 
     key = window.getch()
 
